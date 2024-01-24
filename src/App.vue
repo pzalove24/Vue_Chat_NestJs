6b5089200c876473a78db25058f9c4d138adbbbf
Peeratchai Kleebbua <p-ratchai@hotmail.com>
2024-01-24 20:15:05 +0700

    vue-init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { io } from "socket.io-client"
+import { onBeforeMount, ref } from "vue";
+
+const socket = io('http://localhost:3001')
+const messages = ref([])
+
+onBeforeMount(() => {
+  socket.emit('findAllMessages')
+})
+
+</script>
+
+<template>
+  <div class="chat">
+
+  </div>
+</template>
+
+<style ></style>
